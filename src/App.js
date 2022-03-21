82a7ce564ad6ec20c261f492cec91fd5174f98f9
RafaelAfrikyan <rafaelafrikyan95@gmail.com>
2022-03-21 17:21:43 +0400

    feat: implemetation of Ctrl-Z

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect, useRef } from "react";
+import "./App.css";
 function App() {
+  const [value, setValue] = useState([]);
+  const [result, setResult] = useState([]);
+  const [deleted, setDeleted] = useState([]);
+  useEffect(() => {
+    setResult([...value]);
+  }, [value]);
+  const CtrlZ = () => {
+    setDeleted([...deleted, result.pop()]);
+    setResult([...result]);
+  };
+  const CtrlShiftZ = () => {
+    setResult([...result, deleted.pop()]);
+  };
+  console.log(deleted);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {" "}
+      <input
+        type="text"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      ></input>{" "}
+      <h2>Value - {result}</h2> <button onClick={CtrlZ}>Ctrl+Z</button>{" "}
+      <button onClick={CtrlShiftZ}>Ctrl + Shift + Z</button>{" "}
     </div>
   );
 }
-
 export default App;
